83e2f9630312000c7188641356799eddf2506e90
codingmy <codingmy@naver.com>
2023-11-13 03:13:28 +0900

    [feat] check configData, set default ip, portNum

--- CalcClientEx.java
+++ CalcClientEx.java
@@ -8,8 +8,27 @@ public class CalcClientEx {
         BufferedWriter out = null;
         Socket socket = null;
         Scanner scanner = new Scanner(System.in);
+        String SerIpAddress;
+        String SerPortNum;
+        Config configData = new Config();
+        String defaultIpAdd="localhost";
+        String defaultPortNum="9999";
         try {
-            socket = new Socket("localhost", 9999);
+            
+            //get server's ip, portnum
+            //check whether preset of server data
+            if(configData.getIpAddress()==null || configData.getPortNum()==null)
+            {
+                //setting default data
+                configData.setIpAddress(defaultIpAdd);
+                configData.setPortNum(defaultPortNum);
+            }    
+            //get server's ip, portnum from configuration data
+            SerIpAddress=configData.getIpAddress();
+            SerPortNum=configData.getPortNum();
+
+
+            socket = new Socket(SerIpAddress, Integer.parseInt(SerPortNum));
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             while (true) {
@@ -20,8 +39,11 @@ public class CalcClientEx {
                     out.flush();
                     break; // 사용자가 "bye"를 입력한 경우 서버로 전송 후 연결 종료
                 }
+                //send data to server
                 out.write(outputMessage + "\n"); // 키보드에서 읽은 수식 문자열 전송
                 out.flush();
+
+                //get data from server
                 String inputMessage = in.readLine(); // 서버로부터 계산 결과 수신
                 System.out.println("계산 결과: " + inputMessage);
             }
@@ -38,3 +60,9 @@ public class CalcClientEx {
         }
     }
 }
+
+
+//public void getConfig(String ip, String portNum)
+//{
+    //ip=config.getIpAddress();
+//} 
